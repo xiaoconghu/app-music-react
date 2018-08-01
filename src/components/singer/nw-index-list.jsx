db43504d81d403bdd9be9ae02393cd1422d4a299
追逐萌萌的你 <1216621881@qq.com>
2018-08-01 17:32:25 +0800

    歌手页面初步

---
+++ src/components/singer/nw-index-list.jsx
@@ -0,0 +1,50 @@
+import React, {Component} from "react"
+import './nw-index-list.css'
+
+export class NWIndexList extends Component {
+    state = {
+        data: []
+    };
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        console.log(this.props.data);
+
+    }
+
+    render() {
+        console.log(this.props.data);
+        return (<div style={{width: '100%'}}>
+            <div className="list-view">
+                <ul>
+                    {this.props.data.map((item, index) => (
+                        <li key={index}>
+                            <h2 className="index-list-title">{item.index}</h2>
+                            <ul>
+                                {item.childNode.map((_item, index) => (
+                                    <li key={index} className="singer-l">
+                                        <img className="singerImg" src={_item._Fsinger_mid} alt=""/>
+                                        <span className="singer-name">{_item.Fother_name}</span>
+                                    </li>
+                                ))}
+                            </ul>
+                        </li>
+                    ))}
+
+                </ul>
+            </div>
+
+            <div className="list-shortcut">
+                <ul>
+                    {this.props.data.map((item, index) => (
+                        <li key={index}>{item.index}</li>
+                    ))}
+                </ul>
+            </div>
+
+        </div>)
+    }
+}
--- src/components/singer/singer.jsx
+++ src/components/singer/singer.jsx
@@ -1,10 +1,55 @@
 import React, {Component} from "react"
+import {NWIndexList} from "./nw-index-list";
+import singerService from '../../core/api-server/singer'
+import {CommonUtil} from "../../core/utils/common-util";
 
 export class Singer extends Component {
+    state = {
+        singerData: [{index:'00',childNode:[]}]
+    };
+
     constructor(props) {
         super(props)
     }
-    render(){
-        return (<div>这里是歌手页面</div>)
+
+    componentDidMount() {
+        this.getSingerList()
+    }
+
+    render() {
+        return (<div style={{width:'100%'}}>
+            <NWIndexList data={this.state.singerData}></NWIndexList>
+        </div>)
+    }
+
+    getSingerList() {
+        let data, arr = [], singerData = [];
+
+        let generateBig = CommonUtil.generateBig_1();
+        singerService.getSingerList().then(success => {
+            data = success.data.list;
+            data.forEach((_item, index) => {
+                _item._Fsinger_mid = `https://y.gtimg.cn/music/photo_new/T001R300x300M000${_item.Fsinger_mid}.jpg?max_age=2592000`
+                if (index < 5) {
+                    arr.push(_item);
+                }
+            });
+            singerData.push({index: '热', childNode: arr});
+            generateBig.forEach(item => {
+                let arr = data.filter((_item, index) => {
+                    if (item === _item.Findex) {
+                        return _item
+                    }
+                });
+                let obj;
+                if (data.length > 0) {
+                    obj = {index: item, childNode: arr};
+                }
+                singerData.push(obj)
+            })
+            this.setState({singerData: singerData})
+        });
+
+        console.log(singerData);
     }
 }

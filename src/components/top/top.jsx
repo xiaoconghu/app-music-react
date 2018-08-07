44118a4756c5c505e11f00c63529bf60f2fc5a5a
追逐萌萌的你 <1216621881@qq.com>
2018-08-07 14:41:41 +0800

    排行 搜索 build 配置更改 接口环境配置

--- src/components/top/top.jsx
+++ src/components/top/top.jsx
@@ -52,7 +52,7 @@ class TabExample extends Component {
                     <i className="iconfont icon-yonghu"></i>
                 </div>
                 <Tabs tabs={tabs}
-                      initialPage={3}
+                      initialPage={0}
                       swipeable={true}
                       useOnPan={false}
                       onChange={this.changTab}
